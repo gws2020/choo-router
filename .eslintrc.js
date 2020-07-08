5a9ddab43bcc30f3e5c6ccef3dcca96883bd8d10
jack g <88181159@qq.com>
2020-07-08 10:11:52 +0800

    新增eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,35 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true,
-        'node': true
-    },
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parser':'vue-eslint-parser',
-    'parserOptions': {
-        'parser': '@typescript-eslint/parser',
-        'ecmaVersion': 2018,
-        'sourceType': 'module',
-        'ecmaFeatures': {
-            'legacyDecorators': true
-        }
-    },
-    'plugins': [
-        '@typescript-eslint',
-        'vue'
-    ],
-    'rules': {
-        'no-new': 0,
-        'no-unused-vars': 0,
-        'comma-dangle': [0, 0],
-        'semi': 0,
-        'quotes': [0, 'single'],
-        'no-eval': 1,
-        'no-implied-eval': 1
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser':'vue-eslint-parser',
+  'parserOptions': {
+    'parser': '@typescript-eslint/parser',
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'legacyDecorators': true
     }
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'vue'
+  ],
+  'rules': {
+    'no-new': 0,
+    'no-unused-vars': 0,
+    'comma-dangle': [0, 0],
+    'semi': 0,
+    'quotes': [0, 'single'],
+    'no-eval': 1,
+    'no-implied-eval': 1,
+    "indent": ["error", 2]
+  }
 };
+  
