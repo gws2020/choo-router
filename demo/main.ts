c4810b3e41081d70309469ca6fc52b3c101b8a16
jack g <88181159@qq.com>
2020-07-14 14:14:49 +0800

    build

--- demo/main.ts
+++ demo/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ChooRouter from '../src'
+import ChooRouter from '..'
 
 Vue.use(ChooRouter, {
   router
