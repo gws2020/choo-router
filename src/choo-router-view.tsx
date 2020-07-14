732ea6ce2c151d1ccae082c52d6a46ae1d257d45
jack g <88181159@qq.com>
2020-07-14 12:27:34 +0800

    修改目录结构

--- src/choo-router-view.tsx
+++ src/choo-router-view.tsx
@@ -6,30 +6,26 @@ import { VNode } from 'vue/types/vnode'
 export default class ChooRouterView extends Vue {
 
   public name: string = 'choo-router-view'
-  public path: string = ''
+  public key: string = ''
 
   private get attrs(): {} {
     return Object.assign({}, this.$props, this.$attrs)
   }
 
   public render(h: CreateElement): VNode {
-    const { attrs, path } = this
-    // return h('router-view', {
-    //   attrs,
-    //   key: path
-    // })
-    return <router-view attrs={attrs} key={path}></router-view>
+    const { attrs, key } = this
+    return <router-view attrs={attrs} key={key}></router-view>
   }
 
   @Watch('$route.fullPath', {
     immediate: true
   })
   private routerPathWatch(path: string = '', oldPath: string = '') {
-    const paths: string = this.path.split('?')[0]
+    const paths: string = this.key.split('?')[0]
     path = path.split('?')[0]
     oldPath = oldPath.split('?')[0]
     if (!paths || paths.indexOf(oldPath) !== -1) {
-      this.path = path !== oldPath ? this.path : this.$route.fullPath
+      this.key = path !== oldPath ? this.key : this.$route.fullPath
     }
   }
 }
